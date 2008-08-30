04c6879a6f15b4a869bd914a63ec532787a24e3b
Jarkko Laine <jarkko@jlaine.net>
2008-08-30 20:27:39 +0300

    Made the js to use event delegation

--- public/javascripts/items_index.js
+++ public/javascripts/items_index.js
@@ -8,20 +8,21 @@ Event.addBehavior({
     $('add_form').toggle();
     e.stop();
   },
-  '#add_form' : Remote.Form({onComplete: function() {
-    Event.addBehavior.reload();
-  }}),
-  'input[type=checkbox]:click' : function(e) {
-    var id = this.id.match(/\d{1,}$/);
-    var auth_token = this.up('form').
-                          select('[name=authenticity_token]').
-                          first().value;
-    new Ajax.Request('/items/' + id, 
-                     {asynchronous:true, 
-                      evalScripts:true, 
-                      method: 'put',
-                      parameters: {
-                        authenticity_token: auth_token
-                      }});
-  }
+  '#add_form' : Remote.Form,
+  'ul:click' : Event.delegate({
+    'input[type=checkbox]' : function(e) {
+      var el = e.element();
+      var id = el.id.match(/\d{1,}$/);
+      var auth_token = el.up('form').
+                            select('[name=authenticity_token]').
+                            first().value;
+      new Ajax.Request('/items/' + id, 
+                       {asynchronous:true, 
+                        evalScripts:true, 
+                        method: 'put',
+                        parameters: {
+                          authenticity_token: auth_token
+                        }});
+    }
+  })
 });
